b65bd31bd72df4931cd059487406a8ff22a79c13
dana-savinova <savinovadana@ya.ru>
2020-02-09 20:49:48 +0300

    Создает массив строк случайной длины в объекте

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// данные для объявления
 var HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var HOUSE_ROOMS = [1, 2, 3];
 var NUMBER_GUESTS = [0, 1, 2, 3];
@@ -9,35 +10,60 @@ var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http:
 var OFFER_TIMES = ['12:00', '13:00', '14:00'];
 var OFFERS_NUMBER = 8;
 
+// размеры пина
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
-
+// координаты
 var TOP_BORDER_Y = 130;
 var BOTTOM_BORDER_Y = 630;
 var MIN_Y = TOP_BORDER_Y - PIN_HEIGHT;
 var MAX_Y = BOTTOM_BORDER_Y - PIN_HEIGHT;
 var MIN_X = 0 + PIN_WIDTH / 2;
 
+// необходимые селекторы
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
 var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+// расчет максимальной ширины карты
 // функция для получение размера элемента
 var getElementWidth = function (element) {
   var width = window.getComputedStyle(element).width;
   return parseInt(width, 10);
 };
-
 var mapPinsWidth = getElementWidth(mapPins);
 
 var MAX_X = mapPinsWidth - PIN_WIDTH / 2;
 
-map.classList.remove('map--faded');
-
 // функция для выбора рандомного элемента массива
 var getRandomElement = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
+};
+
+// рандомное число из промежутка
+var getRandomIntInclusive = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
+};
+
+// перемешивание массива
+var getShuffleArray = function (arr) {
+  var j;
+  var temp;
+  for (var i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+};
+
+// Новый массив рандомной длины
+var getArrayRandomLength = function (arr) {
+  return arr.slice(0, getRandomIntInclusive(1, arr.length + 1));
 };
 
 // функция для создания пути к аватару
@@ -64,9 +90,9 @@ var createOffer = function (number) {
     'guests': getRandomElement(NUMBER_GUESTS),
     'checkin': getRandomElement(OFFER_TIMES),
     'checkout': getRandomElement(OFFER_TIMES),
-    'features': getRandomElement(HOUSE_FEATURES),
+    'features': getArrayRandomLength(getShuffleArray(HOUSE_FEATURES)),
     'description': getRandomElement(OFFER_DESCRIPTIONS),
-    'photos': getRandomElement(OFFER_PHOTOS),
+    'photos': getArrayRandomLength(getShuffleArray(OFFER_PHOTOS)),
   },
   'location': {
     'x': getRandomPosition(MIN_X, MAX_X),
@@ -96,6 +122,9 @@ var renderOfferItem = function (offer) {
 
   return offerElement;
 };
+
+// временно переводим карту из неактивного состояния в активное
+map.classList.remove('map--faded');
 
 // вставляем пины
 var renderSimmiliarOffers = function (offers) {
